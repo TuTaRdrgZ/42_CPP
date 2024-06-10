4aa34350ab8f9336bf9623f3299c56997e890d1d
Bautista Rodriguez <bautirodri@gmail.com>
2024-06-10 13:15:05 +0200

    ptr brains

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tuta <bautrodr@student.42barcelona.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/10 13:05:25 by tuta              #+#    #+#             */
+/*   Updated: 2024/06/10 13:13:50 by tuta             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(void)
+{
+    std::string str = "HI THIS IS BRAIN";
+    
+    std::string* stringPTR = &str;
+    
+    std::string& stringREF = str;
+    
+    std::cout << "Memory address of the string variable: " << &str << std::endl;
+    std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
+    std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
+    
+    std::cout << "Value of the string variable: " << str << std::endl;
+    std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+    std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+}
