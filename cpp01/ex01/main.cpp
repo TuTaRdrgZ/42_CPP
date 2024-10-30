67c70401d0d3ead8e2dfdc1b25c93d16a89668b7
Bautista Rodriguez <bautirodri@gmail.com>
2024-10-30 10:55:42 +0100

    added try catch

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tuta <bautrodr@student.42barcelona.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 12:33:47 by tuta              #+#    #+#             */
-/*   Updated: 2024/06/10 13:03:37 by tuta             ###   ########.fr       */
+/*   Updated: 2024/10/30 10:54:51 by tuta             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,17 @@
 
 int main(void)
 {
-	int	N = 5;
+	int	N = 1000000000;
 
 	Zombie *zombie = zombieHorde(N, "asdf");
+	if (zombie == NULL)
+		return (1);
 	for (int i = 0; i < N; i++)
 		zombie[i].announce();
 	delete [] zombie;
 	Zombie *newZombie = zombieHorde(1, "TuTa");
+	if (newZombie == NULL)
+		return (1);
 	newZombie[0].announce();
 	delete [] newZombie;
 	return (0);
--- cpp01/ex01/zombieHorde.cpp
+++ cpp01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: tuta <bautrodr@student.42barcelona.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 12:51:40 by tuta              #+#    #+#             */
-/*   Updated: 2024/06/10 12:59:00 by tuta             ###   ########.fr       */
+/*   Updated: 2024/10/30 10:55:23 by tuta             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,16 @@ Zombie *zombieHorde(int N, std::string name)
 {
 	if (N > 0)
 	{
-		Zombie *zombies = new Zombie[N];
-		for (int i=0; i < N; i++)
-		{
-			zombies[i].set_name(name); 
+		try {
+			Zombie *zombies = new Zombie[N];
+			for (int i=0; i < N; i++)
+				zombies[i].set_name(name); 
+			return (zombies);
 		}
-		return (zombies);
+		catch (std::bad_alloc &e) {
+			std::cerr << "Error: " << e.what() << std::endl;
+			return (NULL);
+		}
 	}
 	return (NULL);
 }
