dd02078ca7e590e720fe646efa0a415610a2546d
Bautista Rodriguez <bautirodri@gmail.com>
2024-11-03 11:11:39 +0100

    test

--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tuta <bautrodr@student.42barcelona.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 16:36:26 by tuta              #+#    #+#             */
-/*   Updated: 2024/07/18 22:29:14 by tuta             ###   ########.fr       */
+/*   Updated: 2024/11/03 11:11:20 by tuta             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,18 @@ int main(void)
     Point c(Fixed(0), Fixed(4));
 
 	// Second triangle
-	Point a1(Fixed(8), Fixed(0));
-    Point b1(Fixed(0), Fixed(8));
-    Point c1(Fixed(0), Fixed(3));
+	Point a1(Fixed(8), Fixed(1));
+    Point b1(Fixed(1.5f), Fixed(8));
+    Point c1(Fixed(2), Fixed(3));
 	
-    Point p(Fixed(1), Fixed(1));
+	// Point 
+    Point p(Fixed(0.5f), Fixed(2));
     Point p1(Fixed(2), Fixed(1));
     Point p2(Fixed(8), Fixed(4));
 
     std::cout << (bsp(a, b, c, p)) << std::endl;
     std::cout << (bsp(a, b, c, p1)) << std::endl;
+	std::cout << (bsp(a1,b1,c1,p1)) << std::endl;
 	std::cout << (bsp(a1,b1,c1,p2)) << std::endl;
 
 	return (0);
