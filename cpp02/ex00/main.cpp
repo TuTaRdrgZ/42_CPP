7fa7c416c735c8ba0f87c2470f3e76aa37710427
Bautista Rodriguez Ramal <bautrodr@car10s6.42barcelona.com>
2024-11-11 08:35:16 +0100

    main

--- cpp02/ex00/main.cpp
+++ cpp02/ex00/main.cpp
@@ -1,19 +1,12 @@
 #include "Fixed.hpp"
-// #include <iomanip>
 
 int main(void)
 {
 	Fixed a;
 	Fixed b( a );
 	Fixed c;
-	// float f= 123456789.0f;
-	// double d = 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 +0.1 + 0.1 + 0.1 + 0.1;
 
 	c = b;
-	// std::cout << std::setprecision(10);
-	// std::cout << "float: " << f << std::endl;
-	// std::cout << std::setprecision(17);
-	// std::cout << "double: " << d << std::endl;
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
