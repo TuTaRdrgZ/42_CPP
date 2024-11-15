500f9f150cea54b7a29d46aa5e3852b3b3680e15
Bautista Rodriguez Ramal <bautrodr@car9s4.42barcelona.com>
2024-11-15 12:29:03 +0100

    main

--- cpp03/ex01/main.cpp
+++ cpp03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tuta <bautrodr@student.42barcelona.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 16:50:32 by tuta              #+#    #+#             */
-/*   Updated: 2024/07/20 13:30:39 by tuta             ###   ########.fr       */
+/*   Updated: 2024/11/15 12:27:41 by bautrodr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,14 @@
 
 int main()
 {
-    // Crear una instancia de ClapTrap
     ClapTrap claptrap("clap-trap");
     
-    // Llamar a los métodos de ClapTrap
     claptrap.attack("target1");
     claptrap.takeDamage(5);
     claptrap.beRepaired(3);
 
-    // Crear una instancia de ScavTrap
     ScavTrap scavtrap("scav-trap");
 
-    // Llamar a los métodos de ScavTrap
     scavtrap.attack("target2");
     scavtrap.takeDamage(30);
     scavtrap.beRepaired(15);
