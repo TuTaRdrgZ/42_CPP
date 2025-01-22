0c5422951776397d7f5d726d880cdde0ba1892e1
Bautista Rodriguez Ramal <bautrodr@cbr6s5.42barcelona.com>
2025-01-22 12:39:16 +0100

    ex02

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -1,17 +1,43 @@
 #pragma once
 
 #include <cstddef>
+#include <stdexcept>
 template <typename T> class Array {
 private:
   T *_data;
   unsigned int _size;
 
 public:
-  Array();
-  Array(unsigned int n);
-  Array(const Array<T> &a);
-  Array &operator=(const Array &t_obj);
-  T operator[](unsigned int n) const;
-  T &operator[](unsigned int n);
-  size_t size() const;
+  Array(void) : _data(NULL), _size(0) {}
+  Array(unsigned int n) : _data(new T[n]), _size(n) {}
+  Array(const Array &other) : _size(other._size) {
+    _data = new T[_size];
+    for (unsigned int i = 0; i < _size; i++)
+      _data[i] = other._data[i];
+  }
+  Array &operator=(const Array &other) {
+    if (this == &other)
+      return (*this);
+    delete[] _data;
+    _size = other._size;
+    _data = new T[_size];
+    for (unsigned int i = 0; i < _size; i++)
+      _data[i] = other._data[i];
+    return (*this);
+  }
+
+  ~Array(void) { delete[] _data; }
+
+  T &operator[](unsigned int index) {
+    if (index >= _size)
+      throw std::out_of_range("Index out of range");
+    return (_data[index]);
+  }
+
+  const T &operator[](unsigned int index) const {
+    if (index >= _size)
+      throw std::out_of_range("Index out of range");
+    return (_data[index]);
+  }
+  unsigned int size() const { return (_size); }
 };
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -2,7 +2,38 @@
 #include <iostream>
 
 int main() {
-  Array<int> a(5);
+
+  {
+    try {
+
+      Array<int> a(5);
+      a[0] = 1;
+      a[1] = 2;
+      a[2] = 3;
+      a[3] = 4;
+      a[4] = 5;
+      a[5] = 6;
+
+      for (unsigned int i = 0; i < a.size(); i++)
+        std::cout << a[i] << std::endl;
+
+    } catch (std::exception &e) {
+      std::cerr << e.what() << std::endl;
+    }
+  }
+  try {
+    Array<std::string> a(3);
+    a[0] = "Hello";
+    a[1] = "World";
+    a[2] = "!";
+
+    for (unsigned int i = 0; i < a.size(); i++)
+      std::cout << a[i] << " ";
+    std::cout << std::endl;
+
+  } catch (std::exception &e) {
+    std::cerr << e.what() << std::endl;
+  }
 
   return 0;
 }
