7895762dfa474869ff88f0a95e779e1945622279
Bautista Rodriguez Ramal <bautrodr@cbr7s6.42barcelona.com>
2025-02-10 10:26:25 +0100

    main

--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -2,17 +2,47 @@
 #include <iostream>
 
 int main(void) {
-  try {
-    Bureaucrat bureaucrat("tuta", 2);
+  {
+    try {
+      Bureaucrat bureaucrat("tuta", 2);
 
-    std::cout << bureaucrat << std::endl;
+      std::cout << bureaucrat << std::endl;
 
-    bureaucrat.incrementGrade();
-    std::cout << bureaucrat.getGrade() << std::endl;
-    bureaucrat.decrementGrade();
-    std::cout << bureaucrat.getGrade() << std::endl;
-  } catch (Bureaucrat::GradeTooHighException &e) {
-    std::cout << e.what() << std::endl;
+      bureaucrat.incrementGrade();
+      std::cout << bureaucrat.getGrade() << std::endl;
+      bureaucrat.decrementGrade();
+      std::cout << bureaucrat.getGrade() << std::endl;
+    } catch (std::exception &e) {
+      std::cout << e.what() << std::endl;
+    }
+  }
+  {
+    try {
+      Bureaucrat bureaucrat("tuta", 150);
+
+      std::cout << bureaucrat << std::endl;
+
+      bureaucrat.decrementGrade();
+      std::cout << bureaucrat.getGrade() << std::endl;
+      bureaucrat.incrementGrade();
+      std::cout << bureaucrat.getGrade() << std::endl;
+    } catch (std::exception &e) {
+      std::cout << e.what() << std::endl;
+    }
+  }
+  {
+    try {
+      Bureaucrat bureaucrat("tuta", 0);
+    } catch (std::exception &e) {
+      std::cout << e.what() << std::endl;
+    }
+  }
+  {
+    try {
+      Bureaucrat bureaucrat("tuta", 151);
+    } catch (std::exception &e) {
+      std::cout << e.what() << std::endl;
+    }
   }
   return 0;
 }
